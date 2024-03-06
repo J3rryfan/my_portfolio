a066740c449a99676831659f55e8dd3f7fe0e253
Jerry Fan <Jerryfan71@gmail.com>
2024-03-05 22:40:10 -0800

    add grid and dot background

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,9 +5,11 @@ import HeroSection from './components/heroSection';
 export default function Home() {
   return (
     <div className=' min-h-screen bg-black overflow-hidden'>
-      <div className=' max-w-7xl mx-auto p-5 '>
-        <Navbar />
-        <HeroSection />
+      <div className='dark:bg-grid-white/[0.05] bg-grid-black/[0.2]'>
+        <div className=' max-w-7xl mx-auto p-5 '>
+          <Navbar />
+          <HeroSection />
+        </div>
       </div>
     </div>
   );
