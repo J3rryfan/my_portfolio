c7f34fa566c9155d805ffec5b0f946614c97e920
Jerry Fan <Jerryfan71@gmail.com>
2024-03-05 22:32:03 -0800

    Add HeroSection component and CSS glow effect

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
 import Navbar from './components/navbar';
+import HeroSection from './components/heroSection';
 
 export default function Home() {
   return (
-    <div className=' min-h-screen bg-black'>
-      <div className=' max-w-7xl mx-auto'>
+    <div className=' min-h-screen bg-black overflow-hidden'>
+      <div className=' max-w-7xl mx-auto p-5 '>
         <Navbar />
+        <HeroSection />
       </div>
     </div>
   );
