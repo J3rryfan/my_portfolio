de2c0c081dcd4a8d9186bf13d4e8ec603ff0320c
Jerry Fan <Jerryfan71@gmail.com>
2024-03-05 22:00:01 -0800

    finished navBar component

---
+++ src/app/(home)/components/navbar.tsx
@@ -0,0 +1,37 @@
+import { Github, Menu, X } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { SiGithub, SiLinkedin } from 'react-icons/si';
+
+export default function Navbar() {
+  const socials = [
+    {
+      Link: 'https://www.linkedin.com/in/jerryfaan/',
+      Label: 'Linkedin',
+      Icon: SiLinkedin,
+    },
+    {
+      Link: 'https://github.com/J3rryfan',
+      Label: 'Github',
+      Icon: SiGithub,
+    },
+  ];
+
+  return (
+    <nav className=' py-10 flex items-center justify-between'>
+      <h1 className=' text-2xl font-bold underline underline-offset-8 decoration-green-600 -rotate-2'>
+        Jerry Fan
+      </h1>
+      <div className=' flex items-center gap-5'>
+        {socials.map((social, index) => {
+          const Icon = social.Icon;
+          return (
+            <Link href={social.Link} key={index} aria-Label={social.Label}>
+              <Icon className='w-5 h-5 hover:scale-125 transition-all' />
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
+}
