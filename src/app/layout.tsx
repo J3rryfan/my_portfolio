5d39a7db81d1f717026e236008b403e059e2d69b
Jerry Fan <Jerryfan71@gmail.com>
2024-03-17 22:56:18 -0700

    Update metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,31 @@ import { ThemeProvider } from '@/components/theme-provider';
 const SpaceGrotesk = Space_Grotesk({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Jerry's Portfolio",
-  description: 'My personal portfolio',
+  metadataBase: new URL('http://localhost:3000/'),
+
+  title: "Jerry Fan's Portfolio",
+
+  authors: {
+    name: 'Jerry Fan',
+  },
+
+  description:
+    "Based in Vancouver, BC. I'm a Fullstack Developer passionate about building a modern web application that users love.",
+  openGraph: {
+    title: "Jerry Fan's Portfolio",
+    description:
+      "Based in Vancouver, BC. I'm a Fullstack Developer passionate about building a modern web application that users love.",
+    url: 'http://localhost:3000/',
+    siteName: "Jerry Fan's Portfolio",
+    images: '/project-profile-1.png',
+    type: 'website',
+  },
+  keywords: [
+    'Frontend Developer',
+    'Jerry Fan',
+    'Portfolio',
+    'Fullstack Developer',
+  ],
 };
 
 export default function RootLayout({
